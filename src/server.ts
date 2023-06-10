6d7a68413a00f2f00c6b405c709fcaa597dc25f2
div <div.hung@gmail.com>
2023-06-10 19:49:49 +0700

    Implement project

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, {Router, Request, Response} from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
@@ -9,7 +9,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -29,18 +29,38 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  app.get( "/filteredimage/", async ( req: Request, res: Response ) => {
+    let { image_url } = req.query;
+    //    1. validate the image_url query
+    if ( !image_url ) {
+      return res.status(400)
+          .send(`image_url is required`);
+    }
+    try {
+      //    2. call filterImageFromURL(image_url) to filter the image
+      let image_path = await filterImageFromURL(image_url);
+      //    3. send the resulting file in the response
+      return res.sendFile(image_path, (err) => {
+        //    4. deletes any files on the server on finish of the response
+        deleteLocalFiles([image_path]);
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
   //! END @TODO1
-  
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
-  
+
 
   // Start the Server
   app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
+    console.log( `server running http://localhost:${ port }` );
+    console.log( `press CTRL+C to stop server` );
   } );
 })();
